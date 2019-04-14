a7bd8f485cf75ddd85136fa9634e17fa04170cbb
Chris Andreotti <chris.andreotti@gmail.com>
2019-04-14 17:57:26 -0400

    Switching the language value in the Button and Field components based on the value of this.context

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Field extends React.Component {
+    static contextType = LanguageContext;
+
     render() {
+        const text = this.context === 'english' ? 'Name' : 'Prénom';
         return(
             <div className="ui field">
-                <label>Name</label>
+                <label>{text}</label>
                 <input />
             </div>
         );
