1f69e6cb8488eca972d9511699095feff7bbc065
Chris Andreotti <chris.andreotti@gmail.com>
2019-04-20 14:23:19 -0400

    Updating the LanguageSelector to use the onLanguageChange callback provided by this.context, instead of this.props

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class LanguageSelector extends React.Component {
+    static contextType = LanguageContext;
+
     render() {
         return (
             <div>
                 Select a language: 
-                <i className="flag us" onClick={() => this.props.onLanguageChange('english')} />
-                <i className="flag fr" onClick={() => this.props.onLanguageChange('french')} />
+                <i className="flag us" onClick={() => this.context.onLanguageChange('english')} />
+                <i className="flag fr" onClick={() => this.context.onLanguageChange('french')} />
             </div>
         );
     }
